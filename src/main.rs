eb470d3a3ce73d8b0a3259d16facb8ddf4b26127
ehitale <elatihe@gmail.com>
2022-09-15 21:16:26 -0500

    added fizzbuzz with match expression

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@ fn main() {
     fb();
 }
 
-fn fizzbuzz() {
+fn fb() {
     //fizzing and buzzing
-};
+    for x in 1..101 {
+        let divis_by_three = x % 3;
+        let divis_by_five = x % 5; 
+
+        let y = (divis_by_three, divis_by_five);
+
+        match y {
+            (0 , 0) => println!("fizzbuzz"),
+            (0 , _) => println!("fizz"),
+            (_, 0) => println!("buzz"),
+            _ => println!("{}", x),
+        };
+    }
+}
