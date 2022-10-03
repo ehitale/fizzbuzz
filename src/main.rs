2afe7a2a5b0f704b8f0f71d309fd420fa7a4d8b6
ehitale <elatihe@gmail.com>
2022-10-02 19:13:48 -0500

    added struct fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+struct Divisable {
+    by_three: bool, 
+    by_five: bool,
+}
+
 enum Divis {
     ByFifteen(i32),
     ByThree(i32),
@@ -11,63 +16,70 @@ fn main() {
     println!("--------");
 
     //fizzbuzz with structs
-    #[allow(dead_code)]
     fb_w_structs();
     println!("--------");
     
     //fizzbuzz with enums
-    #[allow(dead_code)]
     fb_w_enums();
     println!("--------");
 }
 
 fn fb() {
     //fizzing and buzzing
-    for x in 0..100 {
-        let divis_by_three = (x + 1) % 3;
-        let divis_by_five = (x + 1) % 5; 
+    for foo in 0..100 {
+    let divis_by_three = (foo + 1) % 3;
+        let divis_by_five = (foo + 1) % 5; 
 
-        let y = (divis_by_three, divis_by_five);
+        let bar = (divis_by_three, divis_by_five);
 
-        match y {
+        match bar {
             (0 , 0) => println!("fizzbuzz"),
             (0 , _) => println!("fizz"),
             (_, 0) => println!("buzz"),
-            _ => println!("{}", x + 1),
+            _ => println!("{}", foo + 1),
         };
     };
 }
 
 fn fb_w_structs() {
     // I'm just going to copy and paste some code I already did in Repl.it
+    for foo in 0..100 {
+    
+        let bar = Divisable {
+            by_three: if ((foo + 1) % 3) == 0 {true} else {false},
+            by_five: if ((foo + 1) % 5) == 0 {true} else {false},
+        };
+        
+        match (bar.by_three , bar.by_five) {
+            (true, true) => println!("Fizzbuzz"),
+            (true, false) => println!("Fizz"),
+            (false, true) => println!("Buzz"),
+            _ => println!("{}", foo + 1),
+        }
+    }
 }
 
 fn fb_w_enums() {
-    for zed in 0..100 {
-
-
-        {
-            match int_to_divis(zed) {
-                Divis::ByFifteen(n) => println!("{}: Fizzbuzz", n), 
-                Divis::ByThree(n) => println!("{}: Fizz", n), 
-                Divis::ByFive(n) => println!("{}: Buzz", n),
-                Divis::ByNOTB(n) => println!("{}: no ruckus at all!", n),
-            }
+    for foo in 0..100 {
+        match int_to_divis(foo) {
+            Divis::ByFifteen(n) => println!("{}: Fizzbuzz", n), 
+            Divis::ByThree(n) => println!("{}: Fizz", n), 
+            Divis::ByFive(n) => println!("{}: Buzz", n),
+            Divis::ByNOTB(n) => println!("{}: no ruckus at all!", n),
         }
-
     };
 }
 
-fn int_to_divis (zed: i32) -> Divis {
-    let divis_by_three = (zed + 1) % 3;
-    let divis_by_five = (zed + 1) % 5;  
+fn int_to_divis (foo: i32) -> Divis {
+    let divis_by_three = (foo + 1) % 3;
+    let divis_by_five = (foo + 1) % 5;  
     
     let y = (divis_by_three, divis_by_five);
 
     match y {
-        (0, 0) => Divis::ByFifteen(zed + 1),
-        (0, _) => Divis::ByThree(zed + 1),
-        (_, 0) => Divis::ByFive(zed + 1),
-        _ => Divis::ByNOTB(zed + 1),
+        (0, 0) => Divis::ByFifteen(foo + 1),
+        (0, _) => Divis::ByThree(foo + 1),
+        (_, 0) => Divis::ByFive(foo + 1),
+        _ => Divis::ByNOTB(foo + 1),
     }
 }
